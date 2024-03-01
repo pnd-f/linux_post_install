4d1ddb33fceb2c53d927cc81294901f7ffc7f3f8
pnd <pnd-f@ya.ru>
2024-03-01 23:36:40 +0100

    wip

--- python_work_around.py
+++ python_work_around.py
@@ -26,25 +26,47 @@ def suggest_install_python(version: float) -> None:
               f'Do you want to install Python 3.10.')
         answer = input('(y/yes/д/да for confirmation): ')
         if answer in ('y', 'yes', 'д', 'да'):
-            install_python()
-            print('successfully installed! Please restart program, should work =)')
-        exit(0)
+            pass
+        else:
+            exit(0)
 
 
 def install_python() -> None:
-    command1 = (
+    command = (
         'sudo apt install software-properties-common -y '
         '&& sudo add-apt-repository ppa:deadsnakes/ppa '
         '&& sudo apt install python3.10 -y'
     )
-    command2 = 'sudo apt install python3.10'
     try:
-        subprocess.run(command1, shell=True)
+        subprocess.run(command, shell=True)
         # subprocess.run(command2, shell=True)
     except Exception as e:
         print(e)
 
 
+def check_necessary():
+    command = 'python3.10 -V'
+    is_exists = False
+    try:
+        subprocess.run(command, shell=True)
+        is_exists = True
+    except Exception as e:
+        print(e)
+    return is_exists
+
+def re_run():
+    command = 'python3.10 ./run.py'
+    try:
+        subprocess.run(command, shell=True, )
+    except Exception as e:
+        print(e)
+
+
 def check_python_version() -> None:
-    version = get_python_version()
-    suggest_install_python(version)
+    current_version = get_python_version()
+    is_exists_necessary_version = check_necessary()
+    if not is_exists_necessary_version:
+        suggest_install_python(current_version)
+        install_python()
+    re_run()
+
